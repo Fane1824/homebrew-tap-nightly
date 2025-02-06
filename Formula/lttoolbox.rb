01eb04dcabcb657cfde5be498fde9e1d768b6d1b
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-07 01:49:46 +0530

    Changed lttoolbox version

--- Formula/lttoolbox.rb
+++ Formula/lttoolbox.rb
@@ -1,18 +1,18 @@
 class Lttoolbox < Formula
   desc "Finite state compiler, processor and helper tools used by apertium"
   homepage "https://wiki.apertium.org/wiki/Lttoolbox"
-  url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.7.6+g611~071c27e3.orig.tar.bz2"
-  version "3.7.6+g611-071c27e3"
-  sha256 "0c32c8431d2dde1125effa6ac4bc17e1e7dd25eb61e66e69b9c96832661446c0"
+  url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.8.0+g641.648471e4.orig.tar.bz2"
+  version "3.8.0+g641-648471e4"
+  sha256 "ab3528c7ed2348d8baaaef3e1d5bf148bacb3143d52bc6e676a17b6c7a9d6aa1"  
   license "GPL-2.0-or-later"
 
   head "https://github.com/apertium/lttoolbox.git", branch: "main"
 
-  bottle do
-    root_url "https://github.com/WolfPkg/homebrew-tap-nightly/releases/download/lttoolbox-3.7.6+g611-071c27e3"
-    sha256 cellar: :any,                 ventura:      "86539ff5591ce5b2d723c32dde299d354a6d48c32f7a6dd2c05aed5348a620b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ed53da140e40b160013604dabf9303a660f529d2849d46c9e8a41d2c5dd8a82a"
-  end
+#   bottle do
+#     root_url "https://github.com/Fane1824/homebrew-tap-nightly/releases/download/lttoolbox-3.8.0+g641-648471e4"
+#     sha256 cellar: :any, ventura: "REPLACE_WITH_NEW_HASH"
+#     sha256 cellar: :any_skip_relocation, x86_64_linux: "REPLACE_WITH_NEW_HASH"
+#   end
 
   depends_on "cmake" => :build
   depends_on "icu4c"
