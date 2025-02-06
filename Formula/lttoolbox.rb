6b0555ab131766726e268887c8041c928436a608
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-07 02:29:39 +0530

    refactor: update install function attempt 5

--- Formula/lttoolbox.rb
+++ Formula/lttoolbox.rb
@@ -1,39 +1,32 @@
 class Lttoolbox < Formula
-  desc "Finite state compiler, processor and helper tools used by apertium"
-  homepage "https://wiki.apertium.org/wiki/Lttoolbox"
-  url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.8.0+g641~648471e4.orig.tar.bz2",
-    using: :nounzip
-  version "3.8.0+g641-648471e4"
-  sha256 "eb51967ef641fce976ed8db7e70361354b8d909c34fab91d3f9c6c7a89b75719"  
-  license "GPL-2.0-or-later"
-
-  head "https://github.com/apertium/lttoolbox.git", branch: "main"
-
-#   bottle do
-#     root_url "https://github.com/Fane1824/homebrew-tap-nightly/releases/download/lttoolbox-3.8.0+g641-648471e4"
-#     sha256 cellar: :any, ventura: "REPLACE_WITH_NEW_HASH"
-#     sha256 cellar: :any_skip_relocation, x86_64_linux: "REPLACE_WITH_NEW_HASH"
-#   end
-
-  depends_on "cmake" => :build
-  depends_on "icu4c"
-  depends_on "libxml2"
-  depends_on "python3"
-
-  def install
-    system "tar", "-xvf", Dir.glob("#{cached_download}").first
-    extracted_dir = Dir.glob("lttoolbox-*").first
+    desc "Finite state compiler, processor and helper tools used by apertium"
+    homepage "https://wiki.apertium.org/wiki/Lttoolbox"
+    url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.8.0+g641~648471e4.orig.tar.bz2",
+      using: :nounzip
+    version "3.8.0+g641-648471e4"
+    sha256 "eb51967ef641fce976ed8db7e70361354b8d909c34fab91d3f9c6c7a89b75719"  
+    license "GPL-2.0-or-later"
   
-    mkdir "build" do
-      system "cmake", "../#{extracted_dir}", *std_cmake_args
-      system "make"
-      system "ctest"
-      system "make", "install"
+    head "https://github.com/apertium/lttoolbox.git", branch: "main"
+  
+    depends_on "cmake" => :build
+    depends_on "icu4c"
+    depends_on "libxml2"
+    depends_on "python3"
+  
+    def install
+      system "tar", "-xvf", Dir.glob("#{cached_download}").first
+      extracted_dir = Dir.glob("lttoolbox-*.orig").first
+    
+      mkdir "build" do
+        system "cmake", "../#{extracted_dir}", *std_cmake_args
+        system "make"
+        system "ctest"
+        system "make", "install"
+      end
+    end
+  
+    test do
+      system "true"
     end
   end
-  
-
-  test do
-    system "true"
-  end
-end
