f32f804603f6c56513715cd22ef4f4cadfcfa2b9
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-07 02:07:45 +0530

    refactor: update lttoolbox to 3.8.0+g641-648471e4 attempt 6

--- Formula/lttoolbox.rb
+++ Formula/lttoolbox.rb
@@ -4,7 +4,7 @@ class Lttoolbox < Formula
   url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.8.0+g641~648471e4.orig.tar.bz2",
     using: :nounzip
   version "3.8.0+g641-648471e4"
-  sha256 "aeb51967ef641fce976ed8db7e70361354b8d909c34fab91d3f9c6c7a89b75719"  
+  sha256 "eb51967ef641fce976ed8db7e70361354b8d909c34fab91d3f9c6c7a89b75719"  
   license "GPL-2.0-or-later"
 
   head "https://github.com/apertium/lttoolbox.git", branch: "main"
