d871c060f9ca29b889b92f5e2e6cfd40f84211a8
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-08 03:29:29 +0530

    feat: corpustools formula

---
+++ Formula/corpustools.rb
@@ -0,0 +1,22 @@
+class DivvunCorpustools < Formula
+    desc "Divvun tools to manipulate a corpus in different ways"
+    homepage "https://github.com/divvun/CorpusTools"
+    url "https://apertium.projectjj.com/apt/nightly/source/divvun-corpustools/divvun-corpustools_1.3.0+g2607~6ebd9938.orig.tar.bz2" 
+    sha256 "036842cbc527e6400c3f5f80f59001f63c412b8e3df8942caa14dceb88f7a6dd" 
+    version "1.3.0+g2607.6ebd9938" 
+    license "GPL-2.0-or-later"
+  
+    depends_on "python@3.11"
+    depends_on "poetry"
+    depends_on "pkg-config"
+  
+    def install
+      system "poetry", "install", "--without", "dev"
+      bin.install Dir["bin/*"]
+    end
+  
+    test do
+      system "#{bin}/corpustools", "--help"
+    end
+  end
+  
