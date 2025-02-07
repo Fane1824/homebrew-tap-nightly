e62cfbd04709197cadbd9d8f68636c3d3e449350
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-07 15:52:35 +0530

    refactor: update install function attempt 12

--- Formula/lttoolbox.rb
+++ Formula/lttoolbox.rb
@@ -1,34 +1,30 @@
 class Lttoolbox < Formula
-  desc "Finite state compiler, processor and helper tools used by apertium"
-  homepage "https://wiki.apertium.org/wiki/Lttoolbox"
-  url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.8.0+g641~648471e4.orig.tar.bz2"
-  version "3.8.0+g641-648471e4"
-  sha256 "eb51967ef641fce976ed8db7e70361354b8d909c34fab91d3f9c6c7a89b75719"  
-  license "GPL-2.0-or-later"
-
-  head "https://github.com/apertium/lttoolbox.git", branch: "main"
-
-#   bottle do
-#     root_url "https://github.com/Fane1824/homebrew-tap-nightly/releases/download/lttoolbox-3.8.0+g641-648471e4"
-#     sha256 cellar: :any, ventura: "REPLACE_WITH_NEW_HASH"
-#     sha256 cellar: :any_skip_relocation, x86_64_linux: "REPLACE_WITH_NEW_HASH"
-#   end
-
-  depends_on "cmake" => :build
-  depends_on "icu4c"
-  depends_on "libxml2"
-  depends_on "python3"
-
-  def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make"
-      system "ctest"
-      system "make", "install"
+    desc "Finite state compiler, processor and helper tools used by apertium"
+    homepage "https://wiki.apertium.org/wiki/Lttoolbox"
+    version "3.8.0+g641.648471e4" 
+    stable do
+      url "https://apertium.projectjj.com/apt/nightly/source/lttoolbox/lttoolbox_3.8.0+g641~648471e4.orig.tar.bz2"
+      sha256 "eb51967ef641fce976ed8db7e70361354b8d909c34fab91d3f9c6c7a89b75719"
+    end
+  
+    head "https://github.com/apertium/lttoolbox.git", branch: "main"
+  
+    depends_on "cmake" => :build
+    depends_on "icu4c"
+    depends_on "libxml2"
+    depends_on "python3"
+  
+    def install
+      mkdir "build" do
+        system "cmake", "..", *std_cmake_args
+        system "make"
+        system "ctest"
+        system "make", "install"
+      end
+    end
+  
+    test do
+      system "true"
     end
   end
-
-  test do
-    system "true"
-  end
-end
+  
