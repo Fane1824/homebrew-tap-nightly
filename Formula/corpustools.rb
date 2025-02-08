964dbeabb2a5ada020f2dfcc04f68a078d3b1ece
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-08 10:28:49 +0530

    fix: changed corpustools format

--- Formula/corpustools.rb
+++ Formula/corpustools.rb
@@ -1,18 +1,20 @@
 class Corpustools < Formula
     desc "Divvun tools to manipulate a corpus in different ways"
     homepage "https://github.com/divvun/CorpusTools"
-    url "https://apertium.projectjj.com/apt/nightly/source/divvun-corpustools/divvun-corpustools_1.3.0+g2607~6ebd9938.orig.tar.bz2" 
-    sha256 "036842cbc527e6400c3f5f80f59001f63c412b8e3df8942caa14dceb88f7a6dd" 
-    version "1.3.0+g2607.6ebd9938" 
+    url "https://apertium.projectjj.com/apt/nightly/source/divvun-corpustools/divvun-corpustools_1.3.0+g2607~6ebd9938.orig.tar.bz2"
+    sha256 "036842cbc527e6400c3f5f80f59001f63c412b8e3df8942caa14dceb88f7a6dd"
+    version "1.3.0+g2607.6ebd9938"
     license "GPL-3+"
   
     depends_on "python@3.11"
-    depends_on "poetry"
     depends_on "pkg-config"
   
     def install
-      system "poetry", "install", "--no-root", "--no-lock"
-      bin.install Dir["bin/*"]
+      virtualenv_create(libexec, "python3")
+      system libexec/"bin/pip", "install", "-r", "requirements.txt"
+      system libexec/"bin/pip", "install", "."
+  
+      bin.install Dir[libexec/"bin/*"]
     end
   
     test do
