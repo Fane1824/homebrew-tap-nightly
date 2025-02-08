8b5ceaeb91110f2b5f869a9c93f29e7833fdba7f
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-08 10:27:16 +0530

    fix: added no-lock for corpustools

--- Formula/corpustools.rb
+++ Formula/corpustools.rb
@@ -4,14 +4,14 @@ class Corpustools < Formula
     url "https://apertium.projectjj.com/apt/nightly/source/divvun-corpustools/divvun-corpustools_1.3.0+g2607~6ebd9938.orig.tar.bz2" 
     sha256 "036842cbc527e6400c3f5f80f59001f63c412b8e3df8942caa14dceb88f7a6dd" 
     version "1.3.0+g2607.6ebd9938" 
-    license "GPL-2.0-or-later"
+    license "GPL-3+"
   
     depends_on "python@3.11"
     depends_on "poetry"
     depends_on "pkg-config"
   
     def install
-      system "poetry", "install", "--no-root"
+      system "poetry", "install", "--no-root", "--no-lock"
       bin.install Dir["bin/*"]
     end
   
