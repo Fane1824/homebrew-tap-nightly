04496ba01de4ed27f8e1fec7bbdf1cfce831df7c
Ishaan Romil <ishaan.romil@gmail.com>
2025-02-08 10:30:34 +0530

    fix: changed corpustools format 2

--- Formula/corpustools.rb
+++ Formula/corpustools.rb
@@ -1,20 +1,17 @@
 class Corpustools < Formula
+    include Language::Python::Virtualenv
+  
     desc "Divvun tools to manipulate a corpus in different ways"
     homepage "https://github.com/divvun/CorpusTools"
     url "https://apertium.projectjj.com/apt/nightly/source/divvun-corpustools/divvun-corpustools_1.3.0+g2607~6ebd9938.orig.tar.bz2"
     sha256 "036842cbc527e6400c3f5f80f59001f63c412b8e3df8942caa14dceb88f7a6dd"
     version "1.3.0+g2607.6ebd9938"
-    license "GPL-3+"
+    license "GPL-2.0-or-later"
   
     depends_on "python@3.11"
-    depends_on "pkg-config"
   
     def install
-      virtualenv_create(libexec, "python3")
-      system libexec/"bin/pip", "install", "-r", "requirements.txt"
-      system libexec/"bin/pip", "install", "."
-  
-      bin.install Dir[libexec/"bin/*"]
+      virtualenv_install_with_resources
     end
   
     test do
